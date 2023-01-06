07de6b9e1a9b6836c07062330caec154d328f7db
unknown <thotashiva9@gmail.com>
2023-01-07 01:19:07 +0530

    commit for prime and pralindrome

---
+++ pralindrome.js
@@ -0,0 +1,11 @@
+let a = naman;
+let null = "";
+for(let i=a.length;i=>0;i--){
+    null += a[i] 
+    if(a==null){
+console.log("palindrome")
+}
+else
+{
+console.log("Not palindrome")
+}
---
+++ prime.js
@@ -0,0 +1,13 @@
+let n = 10;
+let count = 0;
+for(let i=0;i<=n;i++){
+if(a%i==0){
+count++
+}
+if(count > 2){
+console.log("prime")
+}
+else
+{
+console.log("not prime")
+}
